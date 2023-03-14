f16c985a274de101d00c8550530186248fd3a83e
benesjan <janbenes1234@gmail.com>
2023-03-14 16:42:42 -0600

    deploying without create 2

--- test/GenerateActivity.t.sol
+++ test/GenerateActivity.t.sol
@@ -6,23 +6,26 @@ import {Test} from "forge-std/Test.sol";
 import {Rollup} from "@aztec3/core/Rollup.sol";
 import {Yeeter} from "@aztec3/periphery/Yeeter.sol";
 
+// 1. run: `anvil`
+// 2. run: `forge script --fork-url "http://127.0.0.1:8545/" --ffi GenerateActivityTest --sig "testGenerateActivity()" --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast`
 contract GenerateActivityTest is Test {
-  Rollup internal constant ROLLUP = Rollup(0x01eF16c733AD450180232994c611bA4FADbb7e07);
-  Yeeter internal constant YEETER = Yeeter(0x9BD4B3cF03Cfd4a07c6b0F1470518DAE4bBFa9AC);
+  Rollup internal constant ROLLUP = Rollup(0x5FbDB2315678afecb367f032d93F642f64180aa3);
+  Yeeter internal constant YEETER = Yeeter(0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512);
 
   function _setUp() public {
     // check if there is no code at ROLLUP address
     if (address(ROLLUP).code.length == 0) {
-      address rollup = deployRollupWithCreate2();
+      vm.broadcast();
+      address rollup = address(new Rollup());
       assertEq(address(ROLLUP), rollup, "ROLLUP address mismatch");
+      emit log_named_address("Deployed Rollup to", address(ROLLUP));
     }
     if (address(YEETER).code.length == 0) {
-      address yeeter = deployYeeterWithCreate2();
+      vm.broadcast();
+      address yeeter = address(new Yeeter());
       assertEq(address(YEETER), yeeter, "YEETER address mismatch");
+      emit log_named_address("Deployed Yeeter to", address(YEETER));
     }
-
-    emit log_named_address("ROLLUP", address(ROLLUP));
-    emit log_named_address("YEETER", address(YEETER));
   }
 
   function testGenerateActivity() public {
@@ -35,23 +38,5 @@ contract GenerateActivityTest is Test {
       vm.stopBroadcast();
       emit log_named_uint("Settled block", ROLLUP.latestBlockNum());
     }
-  }
-
-  function deployRollupWithCreate2() public returns (address) {
-    uint256 mySalt = 0xdeadbeef;
-    bytes32 saltHash = keccak256(abi.encodePacked(mySalt));
-
-    vm.broadcast();
-    Rollup rollup = new Rollup{salt: saltHash}();
-    return address(rollup);
-  }
-
-  function deployYeeterWithCreate2() public returns (address) {
-    uint256 mySalt = 0xdeadbeef;
-    bytes32 saltHash = keccak256(abi.encodePacked(mySalt));
-
-    vm.broadcast();
-    Yeeter yeeter = new Yeeter{salt: saltHash}();
-    return address(yeeter);
   }
 }
