7d633b7eeca5925b6a2fe943031192ac6d4f6175
LHerskind <16536249+LHerskind@users.noreply.github.com>
2023-03-27 17:46:42 +0100

    fix: make pure

--- test/Decoder.t.sol
+++ test/Decoder.t.sol
@@ -7,11 +7,11 @@ import {Decoder} from "@aztec3/core/Decoder.sol";
 import {Rollup} from "@aztec3/core/Rollup.sol";
 
 contract DecodeHelper is Decoder {
-  function decode(bytes calldata _l2Block) external returns (uint256, bytes32, bytes32, bytes32) {
+  function decode(bytes calldata _l2Block) external pure returns (uint256, bytes32, bytes32, bytes32) {
     return _decode(_l2Block);
   }
 
-  function computeDiffRoot(bytes calldata _l2Block) external returns (bytes32) {
+  function computeDiffRoot(bytes calldata _l2Block) external pure returns (bytes32) {
     return _computeDiffRoot(_l2Block);
   }
 }
